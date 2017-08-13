5e5cebbcb69844c3925869c8793936257297114d
Jan Greger Hemb <jan.greger@gmail.com>
2017-08-13 21:35:47 +0200

    Added open link for skype and spotify

--- src/app/content-c/offsite-icon/offsite-icon.component.ts
+++ src/app/content-c/offsite-icon/offsite-icon.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
 	selector: 'php-offsite-icon',
@@ -10,11 +11,25 @@ export class OffsiteIconComponent implements OnInit {
 	@Input() offsiteName:string  = "NONE";
 	@Input() offsiteUrl:string  = "NONE";
 	@Input() offsiteImgSRC:string  = "NONE";
+	@Input() offsiteTargetNew:boolean  = true;
 
-	constructor() { }
+	constructor(private sanitizer:DomSanitizer) { }
 
 	ngOnInit() {
 
+	}
+
+	getTargetVal(){
+		if(this.offsiteTargetNew){
+			return '_blank';
+		}else{
+			return '_self';
+		}
+	}
+
+	//https://stackoverflow.com/questions/37432609/how-to-avoid-adding-prefix-unsafe-to-link-by-angular2
+	sanitize(url:string){
+		return this.sanitizer.bypassSecurityTrustUrl(url);
 	}
 
 }
